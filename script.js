8baf2f66e2dd0b5948a33ed879de7665a9e83956
haydenfogel <haydenfogel@ucwv.edu>
2024-09-20 09:56:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitName() 
+{
+    var name = document.getElementById('userName').value; 
+    document.getElementById('displayName').innerText = "WHAT UPPPPPP " + name + "!!!!!!!!!!";
+    document.getElementById('nameField').style.display = 'none';
+}
